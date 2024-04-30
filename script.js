3926b14cd14d85a74e067485f9b29c4ec1fe7f63
jazellemaira <jazellemaira@pm.me>
2024-04-30 22:30:53 +0200

    Adding event listener for the piano keys

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const NOTE_DETAILS = [
+  //variable name is All UPPERCASE since it's a constant variable
   { note: "C", key: "Z", frequency: 261.626 },
   { note: "Db", key: "S", frequency: 277.183 },
   { note: "D", key: "X", frequency: 293.665 },
@@ -12,3 +13,37 @@ const NOTE_DETAILS = [
   { note: "Bb", key: "J", frequency: 466.164 },
   { note: "B", key: "M", frequency: 493.883 },
 ]
+//pressing a key down
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return
+  /*guard clause - if statement that returns from the function early
+                 - always on the top of a function
+                 - ignores the repeat property for keydown */
+  const keyboardKey = e.key.toUpperCase()
+  const noteDetail = getNoteDetail(keyboardKey)
+
+  if (noteDetail == null) return //check if note is equal to null, exit if it is null
+  noteDetail.active = true //if it is in the array, mark it as active - being played
+  playNotes() //function that makes the sound be produced
+})
+
+//releasing a key up
+document.addEventListener("keyup", (e) => {
+  const keyboardKey = e.key.toUpperCase()
+  const noteDetail = getNoteDetail(keyboardKey)
+
+  if (noteDetail == null) return
+  noteDetail.active = false
+  playNotes()
+})
+
+//getting the individual note that is playing based on the key that is pressed
+function getNoteDetail(keyboardKey) {
+  return NOTE_DETAILS.find((n) => n.key === keyboardKey)
+  //find method - finding one individual note from the array of notes
+  //"key${n.key}" - creating an new string of the actual note being checked
+}
+
+function playNotes() {
+  console.log("play notes")
+}
